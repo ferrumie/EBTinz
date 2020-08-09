12504d003a30695c940877c8f3a6ab036a8fec65
Alabi OluwaFeranmi <alabiemmanuelferanmi@gmail.com>
2020-08-09 19:44:44 +0100

    minmal styles

--- tins/urls.py
+++ tins/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('tweet/<int:tweet_id>',views.tweet_detail_view, name='tweet'),
+    path('tin/<int:tweet_id>',views.tin_detail_view, name='tin..'),
+    path('',views.home_view, name='home'),
+    path("tins/", views.tin_list_view, name= "tinslist")
 ]
--- tins/views.py
+++ tins/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from .models import Tins
+import random
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
-    print(args ,kwargs)
-    return HttpResponse("Hi")
+    return render(request,"pages/home.html", context={}, status = 200)
 
-def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    print(args ,kwargs)
-    return HttpResponse(f"Hi,{tweet_id}")
+def tin_detail_view(request, tweet_id, *args, **kwargs):
+    data = {
+        "id":tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tins.objects.get(id = tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not Found"
+        status = 404
+    return JsonResponse(data, status= status)
+
+def tin_list_view(request, *args, **kwargs):
+    qs = Tins.objects.all()
+    tins_list = [{"id":x.id, "content": x.content, "likes":random.randint(0,100)} for x in qs]
+
+    data = {
+        "response":tins_list
+    }
+
+    return JsonResponse(data)
