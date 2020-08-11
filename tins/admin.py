e8d8ed3e62bf36f3c1324427005aaf8ba6b15fc7
Alabi OluwaFeranmi <alabiemmanuelferanmi@gmail.com>
2020-08-11 13:56:41 +0100

    admin search

--- tins/admin.py
+++ tins/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Tins
 # Register your models here.
+
+class TinAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "user"]
+    search_fields = ['content', 'user__username', 'user__email']
+admin.site.register(Tins, TinAdmin)
--- tins/models.py
+++ tins/models.py
@@ -8,10 +8,10 @@ class Tins(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(blank=True, null=True)
-
+    def __str__(self):
+        return self.content
     class Meta:
         ordering = ["-id"]
-
     def serialize(self):
         return {
             "id":self.id,
